f06ad9aec88889b0f3b11ab16c44438fc36121ad
ZoeLiu1999 <zoeyiliu@outlook.com>
2022-04-24 13:02:16 -0700

    Sort

--- sort.sort/src/Sort.java
+++ sort.sort/src/Sort.java
@@ -1,5 +1,5 @@
 public class Sort {
-    public static void sort(String[] x, int start){
+    private static void sort(String[] x, int start){
         if(start==x.length){
             return;
         }
@@ -7,12 +7,12 @@ public class Sort {
         swap(x,start,smallestIndex);
         sort(x,start+1);
     }
-    public static void swap(String[] x, int a, int b){
+    private static void swap(String[] x, int a, int b){
         String temp=x[a];
         x[a]=x[b];
         x[b]=temp;
     }
-    public static int findSmallest(String[] x, int start){
+    private static int findSmallest(String[] x, int start){
         int smallestIndex=start;
         for(int i = 0; i < x.length; i+=1){
             int cmp = x[i].compareTo(x[smallestIndex]);
