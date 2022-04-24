caf87b471537dc5f77efd5ec253dbf48a48bdf79
ZoeLiu1999 <zoeyiliu@outlook.com>
2022-04-24 12:59:19 -0700

    Sort

---
+++ sort.sort/src/Sort.java
@@ -0,0 +1,25 @@
+public class Sort {
+    public static void sort(String[] x, int start){
+        if(start==x.length){
+            return;
+        }
+        int smallestIndex = findSmallest(x,start);
+        swap(x,start,smallestIndex);
+        sort(x,start+1);
+    }
+    public static void swap(String[] x, int a, int b){
+        String temp=x[a];
+        x[a]=x[b];
+        x[b]=temp;
+    }
+    public static int findSmallest(String[] x, int start){
+        int smallestIndex=start;
+        for(int i = 0; i < x.length; i+=1){
+            int cmp = x[i].compareTo(x[smallestIndex]);
+            if(cmp < 0){
+                smallestIndex=i;
+            }
+        }
+        return smallestIndex;
+    }
+}
